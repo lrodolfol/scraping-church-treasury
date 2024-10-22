017694ba27f57ffa2bcaa48193ad244d530fdf46
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-10-21 22:02:07 -0300

    update test with empty values

--- TNN.Test/DomainTest.cs
+++ TNN.Test/DomainTest.cs
@@ -20,12 +20,22 @@ public class DomainTest
         Assert.Equal(MessageErroReturn, exception.Message);
     }
 
+    [Theory]
+    [MemberData(nameof(GetClients))]
+    public void ShoudGenerateExceptionWithEmptyClientValues(string CodeClient, string UserName, string Password, string MessageErroReturn)
+    {
+        var client = () => new UserClient(CodeClient, UserName, Password);
+        var exception = Assert.Throws<DomainException>(client);
+
+        Assert.Equal(MessageErroReturn, exception.Message);
+    }
+
     public static IEnumerable<object[]> GetClients()
     {
         var _fixture = new UserClientTestFixture();
 
-        yield return new object[] { "", _fixture.GetValidUserName(), _fixture.GetValidPassword() };
-        yield return new object[] { _fixture.GetValidCodeClient(), "", _fixture.GetValidPassword() };
-        yield return new object[] { _fixture.GetValidCodeClient(), _fixture.GetValidUserName(), "" };
+        yield return new object[] { "", _fixture.GetValidUserName(), _fixture.GetValidPassword(), "CodeClient can not be null or empty" };
+        yield return new object[] { _fixture.GetValidCodeClient(), "", _fixture.GetValidPassword(), "UserName can not be null or empty" };
+        yield return new object[] { _fixture.GetValidCodeClient(), _fixture.GetValidUserName(), "", "Password can not be null or empty" };
     }
 }
