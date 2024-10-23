3bc16922b452e667d7457da86615068c02c1259d
Rodolfo J.Silva <rodolfo0ti@gmail.com>
2024-10-22 23:01:04 -0300

    Infre project with injection

---
+++ TNN.Infra/TNN.Infra/Config/ValuesConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace TNN.Infra.Config;
+public static class ValuesConfig
+{
+    public static ScrappingValues ScrappingProperties = new();
+    public class ScrappingValues
+    {
+        public string Host { get; set; } = null!;
+        public string UserClient { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ TNN.Infra/TNN.Infra/DI/LoadConfig.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using TNN.Infra.Config;
+using static TNN.Infra.Config.ValuesConfig;
+
+namespace TNN.Infra.Di;
+public class LoadConfig
+{
+    public static IConfigurationRoot CreateConfiguration()
+    {
+        string environment = 
+            Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "dev";
+
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile($"appsettings{environment}.json", false)
+            .Build();
+
+        ConfigurationBridge(configuration);
+
+        return configuration;
+    }
+    public static void EnvironmentConfiguration()
+    {
+        ValuesConfig.ScrappingProperties.UserClient = 
+            Environment.GetEnvironmentVariable("USER_CLIENT") ?? "";
+
+        ValuesConfig.ScrappingProperties.UserName =
+            Environment.GetEnvironmentVariable("USER_NAME") ?? "";
+
+        ValuesConfig.ScrappingProperties.Password =
+            Environment.GetEnvironmentVariable("PASSWORD") ?? "";
+    }
+    public static void ConfigurationBridge(IConfigurationRoot config)
+    {
+        config.GetSection("ScrappingValues").Bind(ValuesConfig.ScrappingProperties);
+    }
+}
